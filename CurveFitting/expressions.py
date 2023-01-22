55a64aea183863fd99e9b446a95b66a412c912fe
Spill-Tea <delrioj17@gmail.com>
2023-01-22 15:20:59 -0800

    a word.

--- CurveFitting/expressions.py
+++ CurveFitting/expressions.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 """
-    CurveFitting/equations.py
+    CurveFitting/epressions.py
 
 """
 # Python Dependencies
