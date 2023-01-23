eff1a19693e1aba40359de3c644c6774820a670a
Spill-Tea <delrioj17@gmail.com>
2023-01-22 17:53:26 -0800

    Goodnes of Fit unit testing.

--- tests/test_goodness.py
+++ tests/test_goodness.py
@@ -6,10 +6,24 @@ import pytest
 import numpy as np
 
 from CurveFitting.goodness_of_fit import Goodness
+from CurveFitting.utils import line
 
 
 @pytest.mark.parametrize("x, y, f, expected", [
-    (np.arange(5), np.arange(5) * 2 - 1, lambda x, m, b: m * x + b, np.array([2., -1])),
+    (np.arange(5), np.arange(5) * 2 - 1, line, np.array([2., -1])),
+])
+def test_dofk(x, y, f, expected):
+    good = Goodness(
+        function=f,
+        xdata=x,
+        ydata=y,
+    )
+    assert good.dof == len(y) == len(x)
+    assert good.k == len(expected)
+
+
+@pytest.mark.parametrize("x, y, f, expected", [
+    (np.arange(5), np.arange(5) * 2 - 1, line, np.array([2., -1])),
 ])
 def test_fit(x, y, f, expected):
     good = Goodness(
