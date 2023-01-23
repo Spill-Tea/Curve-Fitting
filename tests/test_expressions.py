9d07e628d99674963c225f2e7c6f4f8153b21faa
Spill-Tea <delrioj17@gmail.com>
2023-01-22 16:21:58 -0800

    Finalize unit testing of expressions class.

--- tests/test_expressions.py
+++ tests/test_expressions.py
@@ -6,6 +6,39 @@ import pytest
 from CurveFitting import expressions as ex
 
 
+@pytest.mark.parametrize("exp, variables", [
+    (ex.Gaussian, [ex.x]),
+    (ex.Poisson, [ex.x]),
+    (ex.Parabola, [ex.x]),
+    (ex.PadeApproximant, [ex.x]),
+    (ex.OneSiteSpecificBinding, [ex.x]),
+])
+def test_variables(exp: ex.Expression, variables: list):
+    assert exp.variables == variables
+
+
+@pytest.mark.parametrize("exp, constants", [
+    (ex.Gaussian, [ex.mu, ex.sigma]),
+    (ex.Poisson, [ex.mu]),
+    (ex.Parabola, [ex.a, ex.b, ex.c]),
+    (ex.PadeApproximant, [ex.A0, ex.A1, ex.B1]),
+    (ex.OneSiteSpecificBinding, [ex.Bmax, ex.Kd]),
+])
+def test_constants(exp: ex.Expression, constants: list):
+    assert exp.constants == constants
+
+
+@pytest.mark.parametrize("exp, args", [
+    (ex.Gaussian, [ex.x, ex.mu, ex.sigma]),
+    (ex.Poisson, [ex.x, ex.mu]),
+    (ex.Parabola, [ex.x, ex.a, ex.b, ex.c]),
+    (ex.PadeApproximant, [ex.x, ex.A0, ex.A1, ex.B1]),
+    (ex.OneSiteSpecificBinding, [ex.x, ex.Bmax, ex.Kd]),
+])
+def test_args(exp: ex.Expression, args: list):
+    assert exp.args == args
+
+
 @pytest.mark.parametrize("exp, names", [
     (ex.Gaussian, ["x", "mu", "sigma"]),
     (ex.Poisson, ["x", "mu"]),
@@ -15,5 +48,3 @@ from CurveFitting import expressions as ex
 ])
 def test_names(exp: ex.Expression, names: list):
     assert exp.names == names
-
-
