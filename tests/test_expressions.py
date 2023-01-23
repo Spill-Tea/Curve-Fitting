73d8f2f87deb80515d5b9c045ddf18ad872bc205
Spill-Tea <delrioj17@gmail.com>
2023-01-22 23:37:59 -0800

    Remove poisson.

--- tests/test_expressions.py
+++ tests/test_expressions.py
@@ -8,7 +8,6 @@ from CurveFitting import expressions as ex
 
 @pytest.mark.parametrize("exp, variables", [
     (ex.Gaussian, [ex.x]),
-    (ex.Poisson, [ex.x]),
     (ex.Parabola, [ex.x]),
     (ex.PadeApproximant, [ex.x]),
     (ex.OneSiteSpecificBinding, [ex.x]),
@@ -19,7 +18,6 @@ def test_variables(exp: ex.Expression, variables: list):
 
 @pytest.mark.parametrize("exp, constants", [
     (ex.Gaussian, [ex.mu, ex.sigma]),
-    (ex.Poisson, [ex.mu]),
     (ex.Parabola, [ex.a, ex.b, ex.c]),
     (ex.PadeApproximant, [ex.A0, ex.A1, ex.B1]),
     (ex.OneSiteSpecificBinding, [ex.Bmax, ex.Kd]),
@@ -30,7 +28,6 @@ def test_constants(exp: ex.Expression, constants: list):
 
 @pytest.mark.parametrize("exp, args", [
     (ex.Gaussian, [ex.x, ex.mu, ex.sigma]),
-    (ex.Poisson, [ex.x, ex.mu]),
     (ex.Parabola, [ex.x, ex.a, ex.b, ex.c]),
     (ex.PadeApproximant, [ex.x, ex.A0, ex.A1, ex.B1]),
     (ex.OneSiteSpecificBinding, [ex.x, ex.Bmax, ex.Kd]),
@@ -41,7 +38,6 @@ def test_args(exp: ex.Expression, args: list):
 
 @pytest.mark.parametrize("exp, names", [
     (ex.Gaussian, ["x", "mu", "sigma"]),
-    (ex.Poisson, ["x", "mu"]),
     (ex.Parabola, ["x", "a", "b", "c"]),
     (ex.PadeApproximant, ["x", "A0", "A1", "B1"]),
     (ex.OneSiteSpecificBinding, ["x", "Bmax", "Kd"]),
